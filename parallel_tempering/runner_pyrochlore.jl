2c846768c6f101ed0c2410861980987376842a10
zhengbangzhou <zhouzb79@gmail.com>
2024-09-26 13:03:55 -0400

    clean update

--- parallel_tempering/runner_pyrochlore.jl
+++ parallel_tempering/runner_pyrochlore.jl
@@ -1,10 +1,10 @@
 import Pkg
-Pkg.add(path="/home/zhouzb79/projects/def-ybkim/zhouzb79/molecular_dynamics/ClassicalSpinMC.jl")
-Pkg.add("DifferentialEquations")
-Pkg.add("LinearAlgebra")
-Pkg.add("HDF5")
-Pkg.add("Einsum")
-Pkg.add("MPI")
+# Pkg.add(path="/home/zhouzb79/projects/def-ybkim/zhouzb79/molecular_dynamics/ClassicalSpinMC.jl")
+# Pkg.add("DifferentialEquations")
+# Pkg.add("LinearAlgebra")
+# Pkg.add("HDF5")
+# Pkg.add("Einsum")
+# Pkg.add("MPI")
 
 using MPI
 
